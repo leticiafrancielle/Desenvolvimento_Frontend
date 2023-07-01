f0134e95470647a4798cbc4bbf2da9f57b7f36c6
leticiafrancielle <leticiafrancielle2005@gmail.com>
2023-07-01 20:27:42 -0300

    exercícios frontend

---
+++ Listas/Lista_1/exerc_1/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import Analista from './models/analista';
+import Gestor from './models/gestor';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public analistas: Analista[] = new Array<Analista>();
+  public gestor: Gestor = new Gestor("Marcia", 17643, "Gestora de TI", [])
+
+
+  constructor(){
+    this.analistas.push(new Analista("Júlia", 638458, "Analista de infraestrutura", this.gestor.nome));
+    this.analistas.push(new Analista("Ana", 875458, "Analista de sistemas", this.gestor.nome));
+    this.analistas.push(new Analista("Marcelo", 934580, "Analista de cloud", this.gestor.nome));
+    this.analistas.push(new Analista("José", 631265, "Analista de negócios", this.gestor.nome));
+    this.analistas.push(new Analista("Eduarda", 628658, "Analista de sistemas", this.gestor.nome));
+    this.analistas.push(new Analista("Angelo", 552438, "Analista de sistemas", this.gestor.nome));
+    this.analistas.push(new Analista("Marcio", 652638, "Analista de infraestrutura", this.gestor.nome));
+    this.analistas.push(new Analista("Eduardo", 524358, "Analista de cyber-security", this.gestor.nome));
+    this.analistas.push(new Analista("Letícia", 637625, "Analista de infraestrutura", this.gestor.nome));
+    this.analistas.push(new Analista("Francielle", 658762, "Analista de negócios", this.gestor.nome));
+    this.analistas.push(new Analista("Luiza", 138192, "Analista de cloud", this.gestor.nome));
+    this.analistas.push(new Analista("Eric", 258058, "Analista de dados", this.gestor.nome));
+    this.analistas.push(new Analista("Joelma", 608358, "Analista de infraestrutura", this.gestor.nome));
+    this.analistas.push(new Analista("Fabio", 890388, "Analista de cyber-security", this.gestor.nome));
+    this.analistas.push(new Analista("Josenir", 123563, "Analista de dados", this.gestor.nome));
+
+    this.gestor.nomeSubordinados = this.analistas.map(analista => analista.nome);
+  }
+}
---
+++ Listas/Lista_1/exerc_1/src/app/models/gestor.ts
@@ -0,0 +1,20 @@
+import Funcionario from './funcionario';
+
+export default class Gestor implements Funcionario {
+  nome: string;
+  funcional: number;
+  cargo: string;
+  nomeSubordinados: string[];
+
+  constructor(
+    nome: string,
+    funcional: number,
+    cargo: string,
+    nomeSubordinados: string[]
+  ) {
+    this.nome = nome;
+    this.funcional = funcional;
+    this.cargo = cargo;
+    this.nomeSubordinados = nomeSubordinados;
+  }
+}
