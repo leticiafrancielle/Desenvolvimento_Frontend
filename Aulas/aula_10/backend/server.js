2c30351cb025400ea0c3a54a00019300ea8adcb7
leticiafrancielle <leticiafrancielle2005@gmail.com>
2023-08-19 12:08:04 -0300

    adicionando rota de saldo consolidado do dia

--- Aulas/aula_10/backend/server.js
+++ Aulas/aula_10/backend/server.js
@@ -7,6 +7,24 @@ const Pagamento = require('./models/pagamento')
 
 app.use(express.json())
 
+app.get("/listar-saldo-consolidado", async (req, res) => {
+    try {
+        let dataReferencia = (req.query.data)
+
+        let pagamentos = await Pagamento.find({ dataPagamento: dataReferencia })
+
+        let saldoTotalDia = 0
+
+        for (let pagamento of pagamentos) {
+            saldoTotalDia += pagamento.valorPagamento
+        }
+
+        return res.status(200).json({ pagamentos, saldoTotalDia })
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+})
+
 app.get("/listar-pagamentos", async (req, res) => {
     try {
         let pagamentos = await Pagamento.find()
@@ -16,8 +34,15 @@ app.get("/listar-pagamentos", async (req, res) => {
     }
 })
 
-app.post('/cadastrar-pagamento', async (req, res) => {
+app.post("/cadastrar-pagamento", async (req, res) => {
     let pagamento = { ...req.body }
+
+
+    let pagamentoValido = pagamento.tipoPagamento.toUpperCase() != 'C' && pagamento.tipoPagamento.toUpperCase() != "D"
+
+    if (pagamentoValido) {
+        return res.status(400).json('pagamento invalido')
+    }
 
     try {
         await Pagamento.create(pagamento)
@@ -26,6 +51,7 @@ app.post('/cadastrar-pagamento', async (req, res) => {
         return res.status(500).json(error)
     }
 })
+
 
 mongoose.connect(connectionString, {
     dbName: "DevsSupermarket"
